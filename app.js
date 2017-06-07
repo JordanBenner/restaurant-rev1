797379c2c6283655b5220da73b27af87462f2b1c
Jordan Benner <jordanbenner@outlook.com>
2017-06-07 10:39:58 -0500

    Search restaurant id

--- app.js
+++ app.js
@@ -3,33 +3,63 @@ var body_parser = require('body-parser');
 var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: promise
-//
-var db = pgp({database: 'restaurant'});
+});
+var db = pgp({
+  database: 'restaurantv2'
+});
 
 var app = express();
 
-app.set ('view engine', 'hbs');
-const bodyParser = require('body-parser');
-app.use(body_parser.urlencoded({extended: false}));
+app.set('view engine', 'hbs');
+// const bodyParser = require('body-parser');
+app.use(body_parser.urlencoded({
+  extended: false
+}));
 app.use('/static', express.static('public'))
 
-app.post('/submit', function (request, response) {
-  console.log(request.body);
-  response.send('OK');
+app.get('/', function(req, resp, next) {
+  resp.render('homepage.hbs', {});
 });
 
-app.get('/search', function(req, resp, next) {
-  let term = req.query.searchTerm;
-  let query = "SELECT * FROM restaurant WHERE restaurant.name ILIKE '%$1#%'";
-  db.any(query, term)
+app.get('/search', function(request, response, next) {
+  var search = request.query.searchTerm;
+  var query = "SELECT * FROM restaurant WHERE restaurant.name ILIKE '%$1#%'";
+  db.any(query, search)
     .then(function(resultsArray) {
-      resp.render('homepage.hbs', {
+      response.render('search.hbs', {
         results: resultsArray
       });
     })
     .catch(next);
+
+});
+app.get('/restaurant/:id', function(req, resp, next) {
+  var id = request.params.id;
+  var query = "SELECT * FROM restaurant WHERE id = $1";
+  db.any(query, search)
+    .then(function(resultsArray) {
+      response.render('restaurant.hbs', {
+        results: resultsArray
+      });
+    })
+  resp.render('restaurant.hbs', {
+    restaurant: restaurant
+  });
 });
 
-app.listen(8002, function(){
+app.get('/post/:slug', function(request, response) {
+  var slug = request.params.slug;
+  response.send('Post About: ' + slug);
+});
+
+// app.post('/submit', function (request, response) {
+//   console.log(request.body);
+//   response.send('OK');
+// });
+
+
+
+
+app.listen(8002, function() {
   console.log('listening on port 8003')
-})
+});
