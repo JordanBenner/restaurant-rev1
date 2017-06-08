7da81bf94917b94ebaa48ffd9afc652a80e67459
Jordan Benner <jordanbenner@outlook.com>
2017-06-08 09:35:07 -0500

    Login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var body_parser = require('body-parser');
+var session = require(express-session);
 var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: promise
@@ -16,6 +17,22 @@ app.use(body_parser.urlencoded({
   extended: false
 }));
 app.use('/static', express.static('public'))
+app.use(session({
+  secret: process.env.SECTET_KEY || 'dev',
+  resave:true,
+  saveUnititalized: false,
+  cookie: {maxAge: 60000}
+}));
+//redirect to login //
+app.use(function(req, resp, next){
+  if (req.session.user){
+    next();
+  }else if (req.path =='/login'){
+
+  }else {
+        resp.redirect('/login');
+  }
+});
 
 app.get('/', function(req, resp, next) {
   resp.render('homepage.hbs', {});
@@ -51,6 +68,20 @@ app.get('/post/:slug', function(request, response) {
   response.send('Post About: ' + slug);
 });
 
+app.get('/login', function (request, response) {
+  response.render('login.hbs');
+});
+app.post('/login', function (request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username == 'aaron' && password == 'narf') {
+    request.session.user = username;
+    response.redirect('/');
+  } else {
+    response.render('login.hbs');
+  }
+});
+
 // app.post('/submit', function (request, response) {
 //   console.log(request.body);
 //   response.send('OK');
@@ -60,5 +91,5 @@ app.get('/post/:slug', function(request, response) {
 
 
 app.listen(8002, function() {
-  console.log('listening on port 8003')
+  console.log('listening on port 8002')
 });
